7ea9922e7281b36b826832aa99dec79d04242b47
Kushal Mishra <reachkushal05@gmail.com>
2020-12-30 02:58:42 +0530

    ADDED new component for showing charts by selected countries showing covid stats

---
+++ force-app/main/default/lwc/covidStatsByCountryCharts/covidStatsByCountryCharts.js
@@ -0,0 +1,195 @@
+import { LightningElement } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+import D3 from '@salesforce/resourceUrl/d3';
+
+export default class CovidStatsByCountryCharts extends LightningElement {
+
+	scriptLoaded = false;
+	countryValue;
+	countryOptions = [];
+	height = 300;
+	width = 500
+	chartsRendered = false;
+
+	async renderedCallback() {
+		if (!this.scriptLoaded) {
+			this.isLoading = true;
+			await Promise.all([
+				loadScript(this, D3 + '/d3.min.js'),
+			])
+			.then(() => {
+				this.prepareCountryOptions();
+			})
+			.catch((error) => {
+				console.log("Error:", error);
+			});
+			this.scriptLoaded = true;
+		}
+
+		this.isLoading = false;
+	}
+
+	handleCountryChange(event) {
+		const countryVal = event.detail.value;
+		if(countryVal && countryVal.length > 0) {
+			this.chartsRendered = false;
+			this.countryValue = countryVal;
+			this.drawCountryLineChart(this.countryValue);
+			this.chartsRendered = true;
+		}
+	}
+
+	async prepareCountryOptions() {
+		let covidCountries = await fetch('https://api.covid19api.com/countries', {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				}
+			})
+			.then((response) => {
+				return response.json();
+			});
+
+		covidCountries.sort((a,b) => (a.Country > b.Country) ? 1 : ((b.Country > a.Country) ? -1 : 0));;
+		this.countryOptions = covidCountries.map((country) => {
+			return {
+				label: country.Country,
+				value: country.Slug,
+			}
+		});
+		this.countryValue = this.countryOptions[0].value;
+	}
+
+	async drawCountryLineChart(country) {
+		const covidData = await fetch(`https://api.covid19api.com/total/country/${country}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				}
+			})
+			.then((response) => {
+				return response.json();
+			});
+
+		if(covidData && covidData.length > 0) {
+			this.prepareCountryCharts(covidData);
+		}
+	}
+
+	prepareCountryCharts(data) {
+		const confirmedData = this.prepareCountryData(data, "Confirmed");
+		this.prepareLineChart(confirmedData, 'countryChartConfirmed');
+
+		const activeData = this.prepareCountryData(data, "Active");
+		this.prepareLineChart(activeData, 'countryChartActive');
+
+		const recoveredData = this.prepareCountryData(data, "Recovered");
+		this.prepareLineChart(recoveredData, 'countryChartRecovered');
+
+		const deathsData = this.prepareCountryData(data, "Deaths");
+		this.prepareLineChart(deathsData, 'countryChartDeaths');
+	}
+
+	prepareLineChart(data, className) {
+		const d3 = window.d3,
+			margin = {
+				top: 20,
+				right: 30,
+				bottom: 30,
+				left: 70,
+			},
+			bisectDate = d3.bisector(function(d) { return d.date; }).left,
+			formatValue = d3.format(","),
+			x = d3.scaleUtc()
+				.domain(d3.extent(data, d => d.date))
+				.range([margin.left, this.width - margin.right]),
+			y = d3.scaleLinear()
+				.domain([0, d3.max(data, d => d.value)]).nice()
+				.range([this.height - margin.bottom, margin.top]),
+			line = d3.line()
+				.defined(d => !isNaN(d.value))
+				.x(d => x(d.date))
+				.y(d => y(d.value)),
+			yAxis = g => g
+				.attr("transform", `translate(${margin.left},0)`)
+				.call(d3.axisLeft(y))
+				.call(f => f.select(".domain").remove()),
+			xAxis = g => g
+				.attr("transform", `translate(0,${this.height - margin.bottom})`)
+				.call(d3.axisBottom(x).ticks(this.width / 80).tickSizeOuter(0));
+
+		const svgElement = this.template.querySelector(`svg.${className}`);
+		while (svgElement.firstChild) svgElement.removeChild(svgElement.firstChild);
+		const svg = d3.select(svgElement);
+
+		svg.append("g")
+			.call(xAxis);
+
+		svg.append("g")
+			.call(yAxis);
+
+		svg.append("path")
+			.datum(data)
+			.attr("fill", "none")
+			.attr("stroke", "steelblue")
+			.attr("stroke-width", 1.5)
+			.attr("stroke-linejoin", "round")
+			.attr("stroke-linecap", "round")
+			.attr("d", line);
+
+		// eslint-disable-next-line vars-on-top
+		var focus = svg.append("g")
+			.attr("class", "focus")
+			.style("display", "none");
+
+		focus.append("circle")
+			.attr("r", 3);
+
+		focus.append("rect")
+			.attr("class", "tooltip")
+			.attr("width", 70)
+			.attr("height", 50)
+			.attr("x", 10)
+			.attr("y", -22)
+			.attr("rx", 4)
+			.attr("ry", 4);
+
+		focus.append("text")
+			.attr("class", "tooltip-date")
+			.attr("x", 18)
+			.attr("y", -2);
+
+		focus.append("text")
+			.attr("class", "tooltip-count")
+			.attr("x", 18)
+			.attr("y", 18);
+
+		svg.append("rect")
+			.attr("class", "overlay")
+			.attr("width", this.width)
+			.attr("height", this.height)
+			.on("mouseover", function() { focus.style("display", null); })
+			.on("mouseout", function() { focus.style("display", "none"); })
+			.on("mousemove", mousemove);
+
+		function mousemove() {
+			var x0 = x.invert(d3.mouse(this)[0]),
+				i = bisectDate(data, x0, 1),
+				d0 = data[i - 1],
+				d1 = data[i],
+				d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+			focus.attr("transform", "translate(" + x(d.date) + "," + y(d.value) + ")");
+			focus.select(".tooltip-date").text(d.date.toISOString().split('T')[0]);
+			focus.select(".tooltip-count").text(formatValue(d.value));
+		}
+	}
+
+	prepareCountryData(data, type) {
+		return data.map(country => {
+			return {
+				date: new Date(country.Date.slice(0, 10)),
+				value: country[type],
+			}
+		})
+	}
+}
